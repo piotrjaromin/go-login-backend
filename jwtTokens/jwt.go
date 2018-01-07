b6453b3a218ee80328dda807c5d101076b5ad3aa
Piotr Jaromin <pjaromin89@gmail.com>
2018-01-07 20:54:49 +0100

    added fill claims middleware

--- jwtTokens/jwt.go
+++ jwtTokens/jwt.go
@@ -1,66 +1,72 @@
 package jwtTokens
 
 import (
-        "time"
-        "github.com/dgrijalva/jwt-go"
-        "github.com/op/go-logging"
-        "fmt"
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/op/go-logging"
 )
 
 type TokenService struct {
-        Validate      func(token string, claimName string, claimValue string) bool
-        GenerateToken func(username string, userId string) (string, error)
+	Validate      func(token string, claimName string, claimValue string) bool
+	GenerateToken func(username string, userId string) (string, error)
+	GetClaims     func(tokenString string) map[string]interface{}
 }
 
 //Create service which generates and validates jwt tokens
 func Create(signingKey string) TokenService {
-        var log = logging.MustGetLogger("[jwtTokens]")
+	var log = logging.MustGetLogger("[jwtTokens]")
 
-        validate := func(tokenString string, claimName string, claimValue string) bool {
+	getClaims := func(tokenString string) map[string]interface{} {
 
-                token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-                        // Don't forget to validate the alg is what you expect:
-                        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-                                return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-                        }
-                        return []byte(signingKey), nil
-                })
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			// Don't forget to validate the alg is what you expect:
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(signingKey), nil
+		})
 
-                if token == nil {
-                        log.Debugf("Token is nil %+v", tokenString)
-                        return false
-                }
+		if token == nil || err != nil {
+			log.Debugf("Token is nil %+v or error occured %+v", tokenString, err)
+			return map[string]interface{}{}
+		}
 
-                if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-                        log.Debugf("Validating claim %s against value %s, token contiains %s", claimName, claimValue, claims[claimName])
-                        return claims[claimName] == claimValue
-                }
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			return claims
+		}
 
-                log.Errorf("Error while validating token %+v", err)
-                return false
-        }
+		return map[string]interface{}{}
+	}
 
-        generateToken := func(username string, id string) (string, error) {
-                token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-                        "username": username,
-                        "userId": id,
-                        "expiresAt": time.Now().Add(time.Hour * 24 * 7).Unix(),
-                })
+	validate := func(tokenString string, claimName string, claimValue string) bool {
 
-                tokenString, err := token.SignedString([]byte(signingKey))
+		claims := getClaims(tokenString)
+		log.Debugf("Validating claim %s against value %s, token contiains %s", claimName, claimValue, claims[claimName])
+		return claims[claimName] == claimValue
+	}
 
-                if err != nil {
-                        log.Errorf("Could not generate token string %+v", err)
-                        return "", err
-                }
+	generateToken := func(username string, id string) (string, error) {
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username":  username,
+			"userId":    id,
+			"expiresAt": time.Now().Add(time.Hour * 24 * 7).Unix(),
+		})
 
-                return tokenString, nil
-        }
+		tokenString, err := token.SignedString([]byte(signingKey))
 
-        return TokenService{
-                Validate:validate,
-                GenerateToken:generateToken,
-        }
+		if err != nil {
+			log.Errorf("Could not generate token string %+v", err)
+			return "", err
+		}
+
+		return tokenString, nil
+	}
+
+	return TokenService{
+		Validate:      validate,
+		GenerateToken: generateToken,
+		GetClaims:     getClaims,
+	}
 }
-
-
--- security/security.go
+++ security/security.go
@@ -1,14 +1,15 @@
 package security
 
 import (
-	"github.com/piotrjaromin/go-login-backend/jwtTokens"
-	"github.com/piotrjaromin/go-login-backend/web"
-	"github.com/labstack/echo"
-	"github.com/op/go-logging"
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
-	"bytes"
 	"regexp"
+
+	"github.com/labstack/echo"
+	"github.com/op/go-logging"
+	"github.com/piotrjaromin/go-login-backend/jwtTokens"
+	"github.com/piotrjaromin/go-login-backend/web"
 )
 
 const CLAIM_IN_QUOTES_PATTERN = "\".*\""
@@ -21,24 +22,20 @@ func CreateSecurity(tokenService jwtTokens.TokenService) Security {
 	return Security{tokenService: tokenService}
 }
 
-
-func (sec Security) SecuredById(tokenClaimName string, requestClaimName string, claimInBody bool) (func(next echo.HandlerFunc) echo.HandlerFunc) {
+func (sec Security) SecuredById(tokenClaimName string, requestClaimName string, claimInBody bool) func(next echo.HandlerFunc) echo.HandlerFunc {
 
 	var log = logging.MustGetLogger("[Security]")
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 
 			if c.Request().Method() == "OPTIONS" {
-				return nil;
+				return nil
 			}
 
-			authHeader := c.Request().Header().Get("Authorization")
-
-			if len(authHeader) < len("Bearer ") {
+			token, found := getToken(c)
+			if !found {
 				return web.UnauthorizedResponse(c, "Invalid authorization header")
 			}
-
-			token := authHeader[len("Bearer "):]
 
 			var idValue string
 			if claimInBody {
@@ -56,12 +53,17 @@ func (sec Security) SecuredById(tokenClaimName string, requestClaimName string, 
 					return web.LogAndReturnInternalError(c, "[sec]error while unmarshalling request body", err)
 				}
 
-				idValue = string(*jsonMap[requestClaimName])
+				claimValue := jsonMap[requestClaimName]
+				if claimValue == nil {
+					return web.BadRequestResponse(c, "body does not contain required claim "+requestClaimName)
+				}
+
+				idValue = string(*claimValue)
 
 				//string in body contains " at begining and end, remove them
 				claimInQuotes, _ := regexp.MatchString(CLAIM_IN_QUOTES_PATTERN, idValue)
 				if claimInQuotes {
-					idValue = idValue[1:len(idValue) - 1]
+					idValue = idValue[1 : len(idValue)-1]
 				}
 			} else {
 				idValue = c.Param("id")
@@ -73,10 +75,38 @@ func (sec Security) SecuredById(tokenClaimName string, requestClaimName string, 
 				log.Info("saving " + tokenClaimName + " with value " + idValue)
 				c.Set(tokenClaimName, idValue)
 				return next(c)
-			} else {
-				log.Info("invalid claim")
-				return web.UnauthorizedResponse(c, "You do not have required scopes to perform this method")
 			}
+
+			log.Info("invalid claim")
+			return web.UnauthorizedResponse(c, "You do not have required scopes to perform this method")
 		}
 	}
+}
+
+func (sec Security) fillClaims() func(next echo.HandlerFunc) echo.HandlerFunc {
+	var log = logging.MustGetLogger("[Security]")
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+
+			token, found := getToken(c)
+			if found {
+				claims := sec.tokenService.GetClaims(token)
+				for key, value := range claims {
+					c.Set(key, value)
+				}
+			}
+
+			return next(c)
+		}
+	}
+}
+
+func getToken(c echo.Context) (string, bool) {
+	authHeader := c.Request().Header().Get("Authorization")
+
+	if len(authHeader) < len("Bearer ") {
+		return "", false
+	}
+
+	return authHeader[len("Bearer "):], true
 }
